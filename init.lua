e790ff2e2277f129b53f712238573fbb72754df4
masoniis <bott.m@comcast.net>
2022-11-07 21:12:00 -0700

    vscode branch and stuff

--- init.lua
+++ init.lua
@@ -3,23 +3,31 @@ local function import(module)
     return require(module)
 end
 
--- Vanilla Config
-import('settings')
-import('autocmds')
-import('plugins')
-import('keybinds')
-import('colorscheme')
+if vim.g.vscode then
+	import('settings')
+	import('autocmds')
+	import('plugins')
+	import('keybinds')
 
--- vim.notify("What's up, this is an awesome test message!")
+	for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config')..'/lua/plugins', [[v:val =~ '\.lua$']])) do
+		import('plugins.'..file:gsub('%.lua$', ''))
+	end
+else
+	import('settings')
+	import('autocmds')
+	import('plugins')
+	import('keybinds')
+	import('colorscheme')
 
--- Automatically require config in the plugins folder
-for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config')..'/lua/plugins', [[v:val =~ '\.lua$']])) do
-  import('plugins.'..file:gsub('%.lua$', ''))
+	-- Automatically require config in the plugins folder
+	for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config')..'/lua/plugins', [[v:val =~ '\.lua$']])) do
+		import('plugins.'..file:gsub('%.lua$', ''))
+	end
+
+	-- Automatically require configs in the plugins/lsp folder
+	for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config')..'/lua/plugins/lsp', [[v:val =~ '\.lua$']])) do
+		import('plugins.lsp.'..file:gsub('%.lua$', ''))
+	end
+
+	import("plugins.lsp.handlers").setup()
 end
-
--- Automatically require configs in the plugins/lsp folder
-for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config')..'/lua/plugins/lsp', [[v:val =~ '\.lua$']])) do
-  import('plugins.lsp.'..file:gsub('%.lua$', ''))
-end
-
-import("plugins.lsp.handlers").setup()
