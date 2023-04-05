4f70ef30ad048743e15f091d01740f91a6b632e6
masoniis <bott.m@comcast.net>
2023-04-05 12:52:53 -0600

    Fixed up a small leap bind overlap

--- lua/default/plugins/leap.lua
+++ lua/default/plugins/leap.lua
@@ -1,7 +1,10 @@
 return {
 	"ggandor/leap.nvim",
 	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {"tpope/vim-repeat" },
 	config = function ()
 		require('leap').add_default_mappings()
+		vim.api.nvim_del_keymap('v', 'x')
+		vim.api.nvim_del_keymap('v', 'X')
 	end
 }
